4c2a6853132e9f6465e2a6bbbd4fa97403508d0c
neogladium <36886118+neogladium@users.noreply.github.com>
2018-03-05 21:29:20 +0000

    Initial commit

---
+++ js/algoritmo.js
@@ -0,0 +1,25 @@
+/*var $alumnos = 0;
+var $notas;
+
+while ($alumnos <= 2) {
+
+    // $notas = 6;
+    document.write($notas);
+    document.write($alumnos);
+    $alumnos++;
+    document.write($alumnos)
+}
+
+*/
+/*alert("Hola Mundo!");*/
+
+var alumnos;
+var notas;
+var nombre = "";
+
+for (alumnos = 0; alumnos < 34; alumnos++) {
+    // Se ejecuta 5 veces, con valores desde paso desde 0 hasta 4.
+    document.write(notas);
+    document.write(alumnos);
+    document.write(nombre);
+}
