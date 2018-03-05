4376e2af5aa6f9588ae787d54782ad52d97132a2
neogladium <36886118+neogladium@users.noreply.github.com>
2018-03-05 23:14:45 +0000

    Experimentando

--- js/algoritmo.js
+++ js/algoritmo.js
@@ -1,8 +1,9 @@
 //Declaración variables
 var mayor = 0;
+var menor = 0;
 var aprobados = 0;
+//Las notas de los alumnos
 var alumno = [6.5, 4, 5.5, 9, 8.4, 7, 3.6, 4, 5, 8, 9.7, 5, 8.7, 4.2, 7, 5.6, 7.5, 2.5, 4, 6.5, 4.8, 9.8, 6.3, 2.5, 6, 7.8, 4.5, 2, 0, 3, 6.8, 8.6, 7.4, 1, 4.5]
-var mayormenor = new Array();
 
 //Mostrar las notas de los alumnos
 for (i = 0; i < 35; i++) {
@@ -19,6 +20,15 @@ for (i = 0; i < 35; i++) {
 }
 document.write("<br>")
 document.write("La mayor nota es un: " + mayor);
+
+//Mostrar menor nota
+for (i = 0; i < 35; i++) {
+    if (menor > alumno[i]) {
+        menor = alumno[i];
+    }
+}
+document.write("<br>")
+document.write("La menor nota es un: " + menor);
 
 //Alumnos aprobados
 document.write("<br>")
